c4d729f67d977ec17f40281d2e9d0de70b0ca53a
Esteban Herrera V <nabetse.hv.09@gmail.com>
2021-01-03 17:35:42 -0600

    my change

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="Fibonacci"> Fibonacci App </h1> */}
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title"> Fibonacci App </h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
